ee7cb0d702af3e915cadc0a0e7db1a2cff0827c0
mgastonportillo <mgastonportillo@gmail.com>
2022-11-18 01:34:34 -0300

    Fix gap in authentication page

--- src/routes/authentication/authentication.styles.tsx
+++ src/routes/authentication/authentication.styles.tsx
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const AuthPageContainer = styled.div`
 	display: flex;
 	flex-wrap: wrap;
-	gap: 40px;
+	column-gap: 100px;
+	row-gap: 40px;
 	justify-content: center;
 	padding: 0 15px 30px 15px;
 
